5c28acd0ced13c957964ebeda2655f62af9e3123
VaibhavAcharya <vaibhavacharya111@gmail.com>
2020-11-27 15:09:57 +0000

    Deploying to gh-pages from  @ 5a1e80abb603e828c9c2c0b2f1fdec570126bb7f 🚀

---
+++ _next/static/KnDSk_59zZbcbFQ0cYda4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0b052f53eacfbad2fc57.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-64618d76fe98f9678456.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
