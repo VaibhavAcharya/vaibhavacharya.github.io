051dfdf1a9e25bcad73b2721ae584cc54e39e238
Vaibhav Acharya <vaibhavacharya111@gmail.com>
2021-06-05 14:58:22 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -46,23 +46,6 @@ export default function Header() {
       </h1>
       <div className="flex items-center justify-center">
         <div className="flex items-center justify-start space-x-4">
-          <button className="flex items-center justify-between space-x-2 bg-violet-600 hover:bg-violet-700 focus:outline-none focus-visible:bg-violet-800 px-4 py-2 font-bold text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-4 h-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-              />
-            </svg>
-            <span>RESUME</span>
-          </button>
           <div className="flex items-center justify-start space-x-2">
             {icons.map(function ({ Component, color, link, noNewTab }, index) {
               return (
