da20d9e62d85ab178336a3c544e66a873b0471d4
Vaibhav Acharya <vaibhavacharya111@gmail.com>
2022-12-24 21:19:38 +0530

    initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Public Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+  future: {
+    hoverOnlyWhenSupported: true
+  }
+};
