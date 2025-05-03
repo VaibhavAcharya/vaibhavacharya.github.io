75198190cddcafd36680cda070f73d7a3bb7c99a
Vaibhav Acharya <vaibhav@gosupernova.live>
2025-05-03 21:38:57 +0530

    setup astro

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Vaibhav Acharya — engineer + (product & design).";
+export const SITE_DESCRIPTION = "whatever gets us close to product-market fit";
